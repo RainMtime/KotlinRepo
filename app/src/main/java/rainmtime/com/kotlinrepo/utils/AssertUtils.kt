07594f04166f364f5565cf334b26a6c957302eaf
chunyu <jcy23401@163.com>
2018-08-27 15:15:00 +0800

    修改AssertUtils 的实现

--- app/src/main/java/rainmtime/com/kotlinrepo/utils/AssertUtils.kt
+++ app/src/main/java/rainmtime/com/kotlinrepo/utils/AssertUtils.kt
@@ -4,19 +4,17 @@ package rainmtime.com.kotlinrepo.utils
  * Created by chunyu on 2018/8/16 上午10:32.
  * Email: 746431278@qq.com
  */
-class AssertUtils{
+object AssertUtils {
 
-    companion object {
-        fun assertTrue(cond:Boolean){
-            if (!cond){
-                throw  AssertionError()
-            }
+    fun assertTrue(cond: Boolean) {
+        if (!cond) {
+            throw  AssertionError()
         }
+    }
 
-        fun assertTrue(cond:Boolean,msg:String){
-            if (!cond){
-                throw AssertionError(msg)
-            }
+    fun assertTrue(cond: Boolean, msg: String) {
+        if (!cond) {
+            throw AssertionError(msg)
         }
     }
 }
